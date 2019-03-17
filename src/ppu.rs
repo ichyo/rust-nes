0d185a03d9a4d3c8bb0539f7c35087af3f2a4169
ichyo <ichyo00@gmail.com>
2019-03-17 21:51:47 +0900

    Add render method

--- src/ppu.rs
+++ src/ppu.rs
@@ -1,3 +1,4 @@
+use lazy_static::lazy_static;
 use log::{error, trace, warn};
 
 pub struct Ppu {
@@ -10,7 +11,146 @@ pub struct Ppu {
     palette_table: [u8; 0x20],
 }
 
+#[derive(Clone, Copy)]
+pub struct Rgb {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl Rgb {
+    fn new(r: u8, g: u8, b: u8) -> Rgb {
+        Rgb { r, g, b }
+    }
+}
+
+lazy_static! {
+    static ref COLORS: [Rgb; 64] = [
+        Rgb::new(124, 124, 124),
+        Rgb::new(0, 0, 252),
+        Rgb::new(0, 0, 188),
+        Rgb::new(68, 40, 188),
+        Rgb::new(148, 0, 132),
+        Rgb::new(168, 0, 32),
+        Rgb::new(168, 16, 0),
+        Rgb::new(136, 20, 0),
+        Rgb::new(80, 48, 0),
+        Rgb::new(0, 120, 0),
+        Rgb::new(0, 104, 0),
+        Rgb::new(0, 88, 0),
+        Rgb::new(0, 64, 88),
+        Rgb::new(0, 0, 0),
+        Rgb::new(0, 0, 0),
+        Rgb::new(0, 0, 0),
+        Rgb::new(188, 188, 188),
+        Rgb::new(0, 120, 248),
+        Rgb::new(0, 88, 248),
+        Rgb::new(104, 68, 252),
+        Rgb::new(216, 0, 204),
+        Rgb::new(228, 0, 88),
+        Rgb::new(248, 56, 0),
+        Rgb::new(228, 92, 16),
+        Rgb::new(172, 124, 0),
+        Rgb::new(0, 184, 0),
+        Rgb::new(0, 168, 0),
+        Rgb::new(0, 168, 68),
+        Rgb::new(0, 136, 136),
+        Rgb::new(0, 0, 0),
+        Rgb::new(0, 0, 0),
+        Rgb::new(0, 0, 0),
+        Rgb::new(248, 248, 248),
+        Rgb::new(60, 188, 252),
+        Rgb::new(104, 136, 252),
+        Rgb::new(152, 120, 248),
+        Rgb::new(248, 120, 248),
+        Rgb::new(248, 88, 152),
+        Rgb::new(248, 120, 88),
+        Rgb::new(252, 160, 68),
+        Rgb::new(248, 184, 0),
+        Rgb::new(184, 248, 24),
+        Rgb::new(88, 216, 84),
+        Rgb::new(88, 248, 152),
+        Rgb::new(0, 232, 216),
+        Rgb::new(120, 120, 120),
+        Rgb::new(0, 0, 0),
+        Rgb::new(0, 0, 0),
+        Rgb::new(252, 252, 252),
+        Rgb::new(164, 228, 252),
+        Rgb::new(184, 184, 248),
+        Rgb::new(216, 184, 248),
+        Rgb::new(248, 184, 248),
+        Rgb::new(248, 164, 192),
+        Rgb::new(240, 208, 176),
+        Rgb::new(252, 224, 168),
+        Rgb::new(248, 216, 120),
+        Rgb::new(216, 248, 120),
+        Rgb::new(184, 248, 184),
+        Rgb::new(184, 248, 216),
+        Rgb::new(0, 252, 252),
+        Rgb::new(248, 216, 248),
+        Rgb::new(0, 0, 0),
+        Rgb::new(0, 0, 0),
+    ];
+}
+
+struct Sprite([[u8; 8]; 8]);
+
+impl Sprite {
+    fn new() -> Sprite {
+        Sprite([[0; 8]; 8])
+    }
+
+    fn parse(chr: &[u8], index: usize) -> Sprite {
+        let base = index << 4;
+        let mut sprite = Sprite::new();
+        for y in 0..8 {
+            for x in 0..8 {
+                let c1 = chr[base + y] >> (7 - x) & 1;
+                let c2 = chr[base + y + 8] >> (7 - x) & 1;
+                let c = (c2 << 1) | c1;
+                assert!(c <= 3);
+                sprite.0[y][x] = c;
+            }
+        }
+        sprite
+    }
+
+    fn get(&self, x: usize, y: usize) -> u8 {
+        self.0[y][x]
+    }
+}
+
+const WINDOW_HEIGHT: usize = 240;
+const WINDOW_WIDTH: usize = 256;
+
 impl Ppu {
+    pub fn render(&self) -> Vec<Rgb> {
+        let mut res = Vec::new();
+        for y in 0..WINDOW_HEIGHT {
+            for x in 0..WINDOW_WIDTH {
+                let by = y / 8;
+                let bx = x / 8;
+                let ay = y / 16;
+                let ax = x / 16;
+
+                let name_index = by * (WINDOW_WIDTH / 8) + bx;
+                let pattern_index = self.name_table[name_index] as usize;
+
+                let attr_index = 0x3c0 + ay * (WINDOW_WIDTH / 16) + ax;
+                let attr_shift = 4 * (by % 2) + 2 * (bx % 2);
+                let palette_index = (self.name_table[attr_index] >> attr_shift) % 0b11;
+
+                let sprite = Sprite::parse(&self.pattern_table, pattern_index);
+                let sprite_value = sprite.get(x % 8, y % 8);
+                let palette_value = self.pattern_table[(palette_index * 4 + sprite_value) as usize];
+
+                let rgb = COLORS[palette_value as usize];
+                res.push(rgb);
+            }
+        }
+        res
+    }
+
     pub fn new(chr_rom: &[u8]) -> Ppu {
         assert_eq!(chr_rom.len(), 0x2000);
         let mut pattern_table = [0; 0x2000];
